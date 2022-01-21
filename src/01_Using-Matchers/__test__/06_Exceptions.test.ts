edfe85acfafab7667cdecdba8565a439067d4f72
irongineer <daichi.hotchi@gmail.com>
2022-01-22 07:45:44 +0900

    Change capital cases

--- src/01_Using-Matchers/__test__/06_Exceptions.test.ts
+++ src/01_Using-Matchers/__test__/06_Exceptions.test.ts
@@ -2,7 +2,7 @@ function compileAndroidCode() {
   throw new Error('you are using the wrong JDK');
 }
 
-test('compiling android goes as expected', () => {
+test('Compiling android goes as expected', () => {
   expect(() => compileAndroidCode()).toThrow();
   expect(() => compileAndroidCode()).toThrow(Error);
 
